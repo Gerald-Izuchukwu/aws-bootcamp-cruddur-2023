00addc0e2e50d719056740d9fd1eec03a6e5670f
GERALD IZUCHUKWU <geraldlouisugwunna@gmail.com>
2023-04-07 18:44:33 +0000

    query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 # from lib.db import pool
 # from lib.db import pool
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 
 tracer = trace.get_tracer("home.activities")
@@ -18,20 +18,27 @@ class HomeActivities:
     #   span.set_attribute("app.now", now.isoformat())
     #   # span.set_attribute("app.http.method", request.method())
 
-    sql  = """
-    SELECT * FROM activities
-    """
+    sql  = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+    """)
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
         # this will return a tuple
         # the first field being the data
-        json = cur.fetchall()
-    # return json
-      
-    print('2-----')
-    print('3-----')
-    print('4-----')
-    print(json)
-    return json
+        returnedJson = cur.fetchone()
+    return returnedJson[0]
     # return results
